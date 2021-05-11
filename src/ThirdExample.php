7d0d73654c0519910854e9f41fb895ec1bf2b801
Lukas Langen <lukas.langen@check24.de>
2021-05-11 11:53:40 +0200

    Add third practice example

---
+++ src/ThirdExample.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LukasLangen\Workshop\UnitTesting;
+
+use PDO;
+use function mail;
+
+final class ThirdExample
+{
+    // ... more code here
+
+    public function updateEmailForUser(int $id): void
+    {
+        $user = $this->getDb()->query("SELECT * FROM users WHERE id = %id")->fetch(PDO::FETCH_ASSOC);
+
+        // User exists?
+        if ($user) {
+            $newEmail = $_POST['email'];
+
+            if ($newEmail !== $user['email']) {
+                $this->getDb()->exec("UPDATE user SET email=$newEmail WHERE id = $id");
+
+                if (!mail($newEmail, 'Email change successful', 'Your email change was successful')) {
+                    throw new \Exception('Could not send email');
+                }
+            }
+        }
+    }
+
+    // Also used by a lot of other functions
+    private function getDb(): PDO
+    {
+        return new PDO('some-dsn');
+    }
+
+    // ... more code here
+}
